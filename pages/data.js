4c372581937eff89e92726d2ab0a7a232e0e068e
kangza <kangza@umich.edu>
2022-12-28 18:01:19 -0500

    start of data

---
+++ pages/data.js
@@ -0,0 +1,236 @@
+// export const data = [ 
+//     {
+//        "managerName": <string>,
+//        "teams": <map - keys will include players(map), schedule(list),
+//                 record(pair)>,
+//        "schedule": <list of list of pairs>,
+//        "matchWeek": <int>,
+//        "currentTeam": <string>
+//     }
+    
+export const data = [
+    {
+        "managerName": "bob",
+        "teams": [
+            {
+                "faze": [
+                    {
+                        "players": [
+                            {"name": "slasher", "overall": 99},
+                            {"name": "cellium", "overall": 98},
+                            {"name": "simp", "overall": 93},
+                            {"name": "abezy", "overall": 92},
+                        ]
+                    },
+                    {
+                        "schedule": ["breach", "thieves", "ultra", "legion"]
+                    },
+                    {
+                        "record": [0, 0]
+                    }
+                ]
+            },
+            {
+                "breach": [
+                    {
+                        "players": [
+                            {"name": "methodz", "overall": 99},
+                            {"name": "owakening", "overall": 98},
+                            {"name": "nero", "overall": 93},
+                            {"name": "vivid", "overall": 92},
+                        ]
+                    },
+                    {
+                        "schedule": ["breach", "thieves", "ultra", "legion"]
+                    },
+                    {
+                        "record": [0, 0]
+                    }
+                ]
+            },
+            {
+                "mutineers": [
+                    {
+                        "players": [
+                            {"name": "majormaniak", "overall": 99},
+                            {"name": "brack", "overall": 98},
+                            {"name": "havok", "overall": 93},
+                            {"name": "vikul", "overall": 92},
+                        ]
+                    },
+                    {
+                        "schedule": ["faze", "surge", "subliners", "guerillas"]
+                    },
+                    {
+                        "record": [0, 0]
+                    }
+                ]
+            },
+            {
+                "ravens": [
+                    {
+                        "players": [
+                            {"name": "zer0", "overall": 99},
+                            {"name": "paulehx", "overall": 98},
+                            {"name": "asim", "overall": 93},
+                            {"name": "nastie", "overall": 92},
+                        ]
+                    },
+                    {
+                        "schedule": ["subliners", "guerillas", "surge", "optic"]
+                    },
+                    {
+                        "record": [0, 0]
+                    }
+                ]
+            },
+            {
+                "guerillas": [
+                    {
+                        "players": [
+                            {"name": "arcitys", "overall": 99},
+                            {"name": "spart", "overall": 98},
+                            {"name": "huke", "overall": 93},
+                            {"name": "neptune", "overall": 92},
+                        ]
+                    },
+                    {
+                        "schedule": ["breach", "thieves", "ultra", "legion"]
+                    },
+                    {
+                        "record": [0, 0]
+                    }
+                ]
+            },
+            {
+                "thieves": [
+                    {
+                        "players": [
+                            {"name": "octane", "overall": 99},
+                            {"name": "drazah", "overall": 98},
+                            {"name": "kenny", "overall": 93},
+                            {"name": "envoy", "overall": 92},
+                        ]
+                    },
+                    {
+                        "schedule": ["breach", "thieves", "ultra", "legion"]
+                    },
+                    {
+                        "record": [0, 0]
+                    }
+                ]
+            },
+            {
+                "rokkr": [
+                    {
+                        "players": [
+                            {"name": "attach", "overall": 99},
+                            {"name": "cammy", "overall": 98},
+                            {"name": "afro", "overall": 93},
+                            {"name": "bance", "overall": 92},
+                        ]
+                    },
+                    {
+                        "schedule": ["breach", "thieves", "ultra", "legion"]
+                    },
+                    {
+                        "record": [0, 0]
+                    }
+                ]
+            },
+            {
+                "subliners": [
+                    {
+                        "players": [
+                            {"name": "skyz", "overall": 99},
+                            {"name": "priestahh", "overall": 98},
+                            {"name": "hydra", "overall": 93},
+                            {"name": "kismet", "overall": 92},
+                        ]
+                    },
+                    {
+                        "schedule": ["breach", "thieves", "ultra", "legion"]
+                    },
+                    {
+                        "record": [0, 0]
+                    }
+                ]
+            },
+            {
+                "optic": [
+                    {
+                        "players": [
+                            {"name": "dashy", "overall": 99},
+                            {"name": "illey", "overall": 98},
+                            {"name": "scump", "overall": 93},
+                            {"name": "shotzzy", "overall": 92},
+                        ]
+                    },
+                    {
+                        "schedule": ["breach", "thieves", "ultra", "legion"]
+                    },
+                    {
+                        "record": [0, 0]
+                    }
+                ]
+            },
+            {
+                "surge": [
+                    {
+                        "players": [
+                            {"name": "accuracy", "overall": 99},
+                            {"name": "sib", "overall": 98},
+                            {"name": "pred", "overall": 93},
+                            {"name": "mack", "overall": 92},
+                        ]
+                    },
+                    {
+                        "schedule": ["breach", "thieves", "ultra", "legion"]
+                    },
+                    {
+                        "record": [0, 0]
+                    }
+                ]
+            },
+            {
+                "ultra": [
+                    {
+                        "players": [
+                            {"name": "insight", "overall": 99},
+                            {"name": "scrappy", "overall": 98},
+                            {"name": "cleanx", "overall": 93},
+                            {"name": "standy", "overall": 92},
+                        ]
+                    },
+                    {
+                        "schedule": ["breach", "thieves", "ultra", "legion"]
+                    },
+                    {
+                        "record": [0, 0]
+                    }
+                ]
+            },
+            {
+                "legion": [
+                    {
+                        "players": [
+                            {"name": "clayster", "overall": 99},
+                            {"name": "temp", "overall": 98},
+                            {"name": "tjhaly", "overall": 93},
+                            {"name": "prolute", "overall": 92},
+                        ]
+                    },
+                    {
+                        "schedule": ["breach", "thieves", "ultra", "legion"]
+                    },
+                    {
+                        "record": [0, 0]
+                    }
+                ]
+            },
+        ],
+        "schedule": [],
+        "matchweek": 0,
+        "currentTeam": "optic",
+    }
+]
